3e4668ebaa0fe3c0ea4feaffa8cae6b4f5b975b4
gsz <>
2023-02-02 18:26:06 +0800

    0

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div>
+    <h1 style="font-size: 100px">我是你爹！！！</h1>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
